19473ddd8534a2d5b8a2e6756a1680989f5d7f83
linux_china <linux_china@hotmail.com>
2015-04-30 12:08:02 +0800

    spring boot application

---
+++ src/main/java/net/mvnsearch/SpringBootActivemqApplication.java
@@ -0,0 +1,12 @@
+package net.mvnsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActivemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActivemqApplication.class, args);
+    }
+}
